95d24dccf22c5ed156b7e1689d1c85d46138774f
Carolandlancelegacy <Carolandlancelegacy@outlook.com>
2025-08-05 15:01:45 +1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+  const pw = document.getElementById('password').value;
+  if (pw === 'cllegacy') {
+    window.location.href = 'tree.html';
+  } else {
+    document.getElementById('error').innerText = 'Incorrect password';
+  }
+}
